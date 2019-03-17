bc9012a8fe38e327010993d4e3bfc992739cbc2c
sherlock221b <920825209@qq.com>
2019-03-18 00:39:15 +0800

    添加攻击罐子

---
+++ Assets/Scripts/Pot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Animator mAni;
+
+    private void Awake() {
+        this.mAni = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    public void Smash(){
+        this.mAni.SetBool("isDestory",true);
+        StartCoroutine(DestoryPot());
+    }
+
+    private IEnumerator DestoryPot(){
+        yield return new WaitForSeconds(.3f);
+        // GameObject.Destroy(this.gameObject);
+        this.gameObject.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
