0e05d54ec4979c4ac996be68a21cf2ab5e5522e6
sherlock221b <920825209@qq.com>
2019-03-20 01:15:01 +0800

    添加击退

--- Assets/Scripts/Knockback.cs
+++ Assets/Scripts/Knockback.cs
@@ -10,7 +10,10 @@ public class Knockback : MonoBehaviour
 {   
 
     //推的距离
-    public float thrust;
+    public float Thrust;
+
+    //持续时间
+    public float KnockTime;
 
     // Start is called before the first frame update
     void Start()
@@ -25,9 +28,23 @@ public class Knockback : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.CompareTag("breakable")){
-            Debug.Log("attc");
-            other.GetComponent<Pot>().Smash();
+        Debug.Log("发生碰撞..");
+        if(other.CompareTag("enemy")){
+            Rigidbody2D enemyRigidbody = other.GetComponent<Rigidbody2D>();
+            enemyRigidbody.isKinematic = false;   
+            Vector2 diff = gameObject.transform.position - other.transform.position;
+            diff = diff.normalized  * Thrust; 
+            Debug.Log(diff);           
+            enemyRigidbody.AddForce(diff,ForceMode2D.Force);  
+            StartCoroutine(KnockCo(enemyRigidbody));   
+        }
+    }
+
+    private IEnumerator KnockCo(Rigidbody2D enemyRigidbody){
+        if(enemyRigidbody != null){
+            yield return new WaitForSeconds(KnockTime);           
+            enemyRigidbody.velocity = Vector3.zero;
+             enemyRigidbody.isKinematic = true;
         }
     }
 }
